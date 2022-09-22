031c2ea837895027f694f50f29a8c1d9af6da6db
Andrey Trusov <a.trusov@voskhod.com>
2022-09-23 01:57:22 +0400

    Add UI Test Customers

---
+++ src/test/java/ru/papont/rentyourstuff/screen/customer/CustomerBrowseTest.java
@@ -0,0 +1,110 @@
+package ru.papont.rentyourstuff.screen.customer;
+
+import io.jmix.core.DataManager;
+import io.jmix.ui.Screens;
+import io.jmix.ui.component.Table;
+import io.jmix.ui.testassist.UiTestAssistConfiguration;
+import io.jmix.ui.testassist.junit.UiTest;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import ru.papont.rentyourstuff.RentYourStuffApplication;
+import ru.papont.rentyourstuff.customer.Customer;
+import ru.papont.rentyourstuff.entity.Address;
+
+import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@UiTest(authenticatedUser = "admin",
+        mainScreenId = "MainScreen",
+        screenBasePackages = "ru.papont.rentyourstuff.screen.main")
+@ContextConfiguration(classes = {RentYourStuffApplication.class, UiTestAssistConfiguration.class})
+@AutoConfigureTestDatabase
+class CustomerBrowseTest {
+
+    @Autowired
+    DataManager dataManager;
+    private Customer customer;
+
+    @BeforeEach
+    void setUp() {
+        customer = dataManager.create(Customer.class);
+        customer.setFirstName("Foo");
+        customer.setLastName("Bar");
+
+        Address address = dataManager.create(Address.class);
+        address.setStreet("Foo Street 1");
+        address.setCity("Foo City") ;
+        customer.setAddress(address);
+
+         dataManager.save(customer);
+    }
+
+//    @Test
+//    void given_oneCustomerExists_when_openCustomerBrowse_then_tableContainsTheCustomer(Screens screens) {
+//
+//        //given:
+//        CustomerBrowse customerBrowse = openCustomerBrowse(screens);
+//
+//        //expect:
+//        Customer firstCustomer = firstLoadedCustomer(customerBrowse);
+//
+//        assertThat(firstCustomer)
+//                .isEqualTo(customer);
+//
+//        assertThat(firstCustomer.getFirstName() )
+//                .isEqualTo(customer.getFirstName());
+//
+//
+//    }
+
+    @Test
+    void given_oneCustomerExists_when_openCustomerBrowse_then_tableContainsTheCustomer(Screens screens) {
+
+        //given:
+        CustomerBrowse customerBrowse = openCustomerBrowse(screens);
+
+        assertThat(firstLoadedCustomer(customerBrowse))
+                .isEqualTo(customer);
+
+    }
+
+
+    @NotNull
+    private Customer firstLoadedCustomer(CustomerBrowse customerBrowse) {
+        Collection<Customer> customers = loadedCustomers(customerBrowse);
+        return customers.stream().findFirst().orElseThrow();
+    }
+
+    @NotNull
+    private CustomerBrowse openCustomerBrowse(Screens screens) {
+        CustomerBrowse customerBrowse = screens.create(CustomerBrowse.class);
+        customerBrowse.show();
+        return customerBrowse;
+    }
+
+    @NotNull
+    private Collection<Customer> loadedCustomers(CustomerBrowse customerBrowse) {
+        Table<Customer> customersTable = customersTable(customerBrowse);
+        return customersTable.getItems().getItems();
+    }
+
+    @Nullable
+    private Table<Customer> customersTable(CustomerBrowse customerBrowse) {
+        return (Table<Customer>) customerBrowse.getWindow().getComponent("customersTable");
+    }
+
+
+    @AfterEach
+    void tearDown() {
+        dataManager.remove(customer );
+    }
+}
